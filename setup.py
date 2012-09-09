911031764652df1de713b15a2b48c046636de2c6
Chris Priest <cp368202@ohiou.edu>
2012-09-09 15:59:13 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='giotto',
+    version='0.0.1',
+    description='Python Distribution Utilities',
+    author='Chris Priest'
+    author_email='cp368202@ohiou.edu',
+    url='https://github.com/priestc/giotto',
+    packages=['giotto'],
+)
