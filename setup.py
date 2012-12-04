b77bda2ffdd022559cc742406c1c2be137f7218c
Chris Priest <cp368202@ohiou.edu>
2012-12-03 19:50:30 -0500

    made 0.1.0 release

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@ from setuptools import setup
 
 setup(
 	name='giotto',
-    version='0.0.3pre',
-    description='MVC microframework',
+    version='0.1.0',
+    description='MVC Application Framework',
     long_description=open('README.md').read(),
     author='Chris Priest',
     author_email='cp368202@ohiou.edu',
     url='https://github.com/priestc/giotto',
-    packages=['giotto','giotto.controllers','giotto.contrib.auth','giotto.views'],
+    packages=[
+        'giotto',
+        'giotto.controllers',
+        'giotto.contrib.auth',
+        'giotto.contrib.static',
+        'giotto.views'
+    ],
     scripts=['giotto/giotto_project'],
     license='LICENSE',
     install_requires=['werkzeug', 'python-magic', 'jinja2', 'py-bcrypt', 'decorator'],
