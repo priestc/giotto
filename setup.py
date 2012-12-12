31f41279955c2eae77725612999670ebf70f1479
Chris Priest <cp368202@ohiou.edu>
2012-12-12 16:54:12 -0500

    upped versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='giotto',
-    version='0.9.0',
+    version='0.9.1',
     description='MVC Application Framework',
     long_description=open('README.md').read(),
     author='Chris Priest',
