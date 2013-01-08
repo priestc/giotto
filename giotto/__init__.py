dc2657a429d41b827a247e3dc307aa67d0ccfff0
Chris Priest <cp368202@ohiou.edu>
2013-01-08 02:23:48 -0500

    bumped version numbers

--- giotto/__init__.py
+++ giotto/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.10.1'
+__version__ = '0.10.2'
 import giotto.controllers
 import giotto.views
 import giotto.keyvalue
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='giotto',
-    version='0.10.1',
+    version='0.10.2',
     description='Web development simplified. An MVC framework supporting Python 3.',
     long_description=open('README.rst').read(),
     author='Chris Priest',
