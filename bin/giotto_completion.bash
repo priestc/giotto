83678de91b22b33c344224df3740e273f019f9a1
Chris Priest <cp368202@ohiou.edu>
2013-01-13 01:57:47 -0500

    added bash completion

---
+++ bin/giotto_completion.bash
@@ -0,0 +1,13 @@
+_giotto() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    if [[ ${prev} == cmd ]] ; then
+        COMPREPLY=( $(giotto suggest ${cur}) )
+        return 0
+    fi
+}
+complete -F _giotto giotto
