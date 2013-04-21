42920843c91622feeae853708d284fc6dc2f8b2a
Chris Priest <cp368202@ohiou.edu>
2013-04-20 23:05:08 -0400

    added post_register_callback kwarg to the register manifest macro

--- giotto/contrib/auth/manifest.py
+++ giotto/contrib/auth/manifest.py
@@ -1,18 +1,31 @@
+from giotto import get_config
 from giotto.programs import GiottoProgram, ProgramManifest
 from giotto.views import GiottoView, BasicView, jinja_template
 from giotto.control import Redirection
 
 from .middleware import NotAuthenticatedOrRedirect, AuthenticationMiddleware, NotAuthenticatedOrDie, LogoutMiddleware
-from .models import create_session, basic_register
+from .models import create_session, basic_register, User
 
-def create_auth_manifest(*args):
+def create_auth_manifest(**kwargs):
     """
     Creates a basic authentication manifest for logging in, logging out and
     registering new accounts.
     """
-
     class AuthProgram(GiottoProgram):
         pre_input_middleware = [AuthenticationMiddleware]
+
+    def register(username, password, password2):
+        """
+        Decorated version of basic_register with a callback added.
+        """
+        result = basic_register(username, password, password2)
+        callback = kwargs.pop('post_register_callback', None)
+        if callback:
+            session = get_config('session')
+            user = session.query(User).filter_by(username=username).first()
+            callback(user)
+        return result
+
     return ProgramManifest({
         'login': [
             AuthProgram(
@@ -46,7 +59,7 @@ def create_auth_manifest(*args):
             ),
             AuthProgram(
                 controllers=['http-post'],
-                model=[basic_register],
+                model=[register],
                 view=BasicView(
                     html=lambda m: Redirection('/', persist={'giotto_session': m['session_key']}),
                 ),
--- giotto/control.py
+++ giotto/control.py
@@ -2,7 +2,7 @@ class GiottoControl(object):
     pass
 
 class Redirection(GiottoControl):
-    # because render functions have this defined, and this onject can be used in lieu
+    # because render functions have this defined, and this object can be used in lieu
     # of a render function.
     __name__ = '__Redirection'
     def __init__(self, path, persist={}):
